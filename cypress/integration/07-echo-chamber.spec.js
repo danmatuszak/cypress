daf85741d8d51f4dabf9e30e642b960c62d5fccd
danmatuszak <dmatuszak@ucdavis.edu>
2023-06-09 18:06:04 -0700

    add form validation

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -27,11 +27,30 @@ describe('Initial Page', () => {
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-submit"]').as('submit')
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submit').click()
+    // if pop up, this should be pseudo element
+    // cy.get('[data-test="sign-up-email"]:invalid')
+    //   .invoke('prop', 'validationMessage')
+    //   .should('contain', 'Please fill out this field')
+
+    // another way
+    cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true')
+  });
 
   it('should require that the email actually be an email address', () => {});
 
-  it('should require a password when the email is present', () => {});
+  it('should require a password when the email is present', () => {
+    cy.get('[data-test="sign-up-email"]').type('test@test.com{enter}');
+
+    cy.get('[data-test="sign-up-password"]:invalid')
+      .invoke('prop', 'validationMessage')
+      .should('contain', 'Please fill out this field.')
+  });
 });
